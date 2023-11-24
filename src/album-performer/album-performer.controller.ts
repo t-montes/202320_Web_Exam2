4b677eaefec7a3c74703b28a80a641839533c824
t-montes <t.montes@uniandes.edu.co>
2023-11-24 14:17:04 -0500

    association

--- src/album-performer/album-performer.controller.ts
+++ src/album-performer/album-performer.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+/**
+ * 
+ */
 
-@Controller('album-performer')
-export class AlbumPerformerController {}
+import { Controller, Param, Post } from '@nestjs/common';
+import { UseInterceptors } from '@nestjs/common';
+import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors.interceptor';
+import { AlbumPerformerService } from './album-performer.service';
+
+@Controller('albums/:albumId/performers')
+export class AlbumPerformerController {
+    constructor(private readonly albumPerformerService: AlbumPerformerService) {}
+
+    @Post(':performerId')
+    @UseInterceptors(BusinessErrorsInterceptor)
+    async create(@Param('albumId') albumId: string, @Param('performerId') performerId: string) {
+        return await this.albumPerformerService.addPerformerToAlbum(albumId, performerId);
+    }
+}
